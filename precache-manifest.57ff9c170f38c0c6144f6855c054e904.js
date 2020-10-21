1a8faf6f9badb1c708590ee8fc2286e23aac751c
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-10-21 18:23:09 +0400

    Updates

---
+++ precache-manifest.57ff9c170f38c0c6144f6855c054e904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839278560d1da296459bbac32610fc9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc07743aa2e3796de5c",
+    "url": "/static/css/main.98a1224a.chunk.css"
+  },
+  {
+    "revision": "6b1c092fe2e628e62f5b",
+    "url": "/static/js/2.039112ce.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.039112ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc07743aa2e3796de5c",
+    "url": "/static/js/main.9e92b693.chunk.js"
+  },
+  {
+    "revision": "c92001c23c773bc0da46",
+    "url": "/static/js/runtime-main.d8ad46b0.js"
+  }
+]);
